be1bdd81e13023d913542747f541437348a18a5b
Jonathan Kuck <jdkuck@gmail.com>
2015-11-29 15:44:40 -0800

    saves .fits image planes as raw bytes in a text file

---
+++ fitsToText.cpp
@@ -0,0 +1,93 @@
+//To compile and run:
+// On os x:
+// g++ fitsToText.cpp -g -I ./include -I DarwinX86/pex_policy/10.1+1/include/ -I DarwinX86/daf_persistence/10.1+1/include/ -I DarwinX86/utils/10.1+1/include/ -I DarwinX86/daf_base/10.1+2/include/ -I DarwinX86/base/10.1+1/include/ -I DarwinX86/ndarray/10.1+2/include/ -I DarwinX86/pex_exceptions/10.1+1/include/ -I DarwinX86/eigen/3.2.0/include/ -I DarwinX86/afw/10.1+1/include -L ./bin -L DarwinX86/afw/10.1+1/lib -L DarwinX86/daf_base/10.1+2/lib/ -L DarwinX86/daf_persistence/10.1+1/lib/ -L DarwinX86/boost/1.55.0.1.lsst2+3/lib/ -lHalide -lafw -ldaf_base -ldaf_persistence -lboost_system `libpng-config --cflags --ldflags` -o fitsToText -std=c++11
+//`libpng-config --cflags --ldflags` removed below:
+// g++ fitsToText.cpp -g -I ./include -I DarwinX86/pex_policy/10.1+1/include/ -I DarwinX86/daf_persistence/10.1+1/include/ -I DarwinX86/utils/10.1+1/include/ -I DarwinX86/daf_base/10.1+2/include/ -I DarwinX86/base/10.1+1/include/ -I DarwinX86/ndarray/10.1+2/include/ -I DarwinX86/pex_exceptions/10.1+1/include/ -I DarwinX86/eigen/3.2.0/include/ -I DarwinX86/afw/10.1+1/include -L ./bin -L DarwinX86/afw/10.1+1/lib -L DarwinX86/daf_base/10.1+2/lib/ -L DarwinX86/daf_persistence/10.1+1/lib/ -L DarwinX86/boost/1.55.0.1.lsst2+3/lib/ -lHalide -lafw -ldaf_base -ldaf_persistence -lboost_system -o fitsToText -std=c++11
+//
+// DYLD_LIBRARY_PATH=./bin:DarwinX86/afw/10.1+1/lib/:DarwinX86/daf_persistence/10.1+1/lib/:DarwinX86/daf_base/10.1+2/lib/:DarwinX86/boost/1.55.0.1.lsst2+3/lib/:DarwinX86/xpa/2.1.15.lsst2/lib/:DarwinX86/pex_policy/10.1+1/lib/:DarwinX86/pex_logging/10.1+1/lib/:DarwinX86/utils/10.1+1/lib/:DarwinX86/pex_exceptions/10.1+1/lib/:DarwinX86/base/10.1+1/lib/ ./fitsToText
+//
+// On linux:
+// g++ fitsToText.cpp -g -I ./include -I Linux64/pex_policy/10.1+1/include/ -I Linux64/daf_persistence/10.1+1/include/ -I Linux64/utils/10.1+1/include/ -I Linux64/daf_base/10.1+2/include/ -I Linux64/base/10.1+1/include/ -I Linux64/ndarray/10.1+2/include/ -I Linux64/pex_exceptions/10.1+1/include/ -I Linux64/eigen/3.2.0/include/ -I Linux64/afw/10.1+1/include -L ./bin -L Linux64/afw/10.1+1/lib -L Linux64/daf_base/10.1+2/lib/ -L Linux64/daf_persistence/10.1+1/lib/ -L Linux64/boost/1.55.0.1.lsst2+3/lib/ -L Linux64/wcslib/4.14+7/lib/ -lHalide -lafw -ldaf_base -ldaf_persistence -lboost_system `libpng-config --cflags --ldflags` -lpthread -ldl -o fitsToText -std=c++11
+//
+// LD_LIBRARY_PATH=./bin:Linux64/afw/10.1+1/lib/:Linux64/daf_persistence/10.1+1/lib/:Linux64/daf_base/10.1+2/lib/:Linux64/boost/1.55.0.1.lsst2+3/lib/:Linux64/xpa/2.1.15.lsst2/lib/:Linux64/pex_policy/10.1+1/lib/:Linux64/pex_logging/10.1+1/lib/:Linux64/utils/10.1+1/lib/:Linux64/pex_exceptions/10.1+1/lib/:Linux64/base/10.1+1/lib/:Linux64/wcslib/4.14+7/lib/:Linux64/cfitsio/3360.lsst1/lib/:Linux64/gsl/1.16.lsst1/lib/:Linux64/minuit2/5.28.00/lib:Linux64/mysql/5.1.65.lsst2/lib/ ./fitsToText
+
+//This program takes a fits image and writes the three planes to text files
+//Each row (same y value) is on the same lines
+//x values within 
+
+#ifndef STANDALONE
+#include "lsst/afw/image.h"
+namespace afwImage = lsst::afw::image;
+namespace afwMath  = lsst::afw::math;
+#endif
+
+#include <stdio.h>
+#include "Halide.h"
+#include <bitset>
+#include "clock.h"
+using namespace std;
+using namespace Halide;
+
+using Halide::Image;
+
+int main(int argc, char *argv[]) {
+
+#ifndef STANDALONE
+    auto im = afwImage::MaskedImage<float>("./images/calexp-0288976_24.fits");
+    int width = im.getWidth(), height = im.getHeight();
+#else
+    int width = 2048, height = 1489;
+    printf("[no load]");
+#endif
+    printf("Loaded: %d x %d\n", width, height);
+
+    Image<float> image(width, height);
+    Image<float> variance(width, height);
+    Image<uint16_t> mask(width, height);
+
+  float* imageBuffer = (float*)malloc(sizeof(float)*width*height);
+  float* varianceBuffer = (float*)malloc(sizeof(float)*width*height);
+  uint16_t* maskBuffer = (uint16_t*)malloc(sizeof(uint16_t)*width*height);
+
+
+#ifndef STANDALONE
+    //Read image in
+    for (int y = 0; y < height; y++) {
+        afwImage::MaskedImage<float, lsst::afw::image::MaskPixel, lsst::afw::image::VariancePixel>::x_iterator inPtr = im.x_at(0, y);
+        for (int x = 0; x < width; x++){
+            imageBuffer[x + y*width] = (*inPtr).image();
+            varianceBuffer[x + y*width] = (*inPtr).variance();
+            maskBuffer[x + y*width] = (*inPtr).mask();
+            inPtr++;
+        }
+    }
+#endif
+
+    FILE * imgFileP;
+    imgFileP = fopen ("./warpRawData/input.img", "wb");
+    fwrite (imageBuffer , sizeof(float), width*height, imgFileP);
+    fclose (imgFileP);
+
+    FILE * varFileP;
+    varFileP = fopen ("./warpRawData/input.var", "wb");
+    fwrite (varianceBuffer , sizeof(float), width*height, varFileP);
+    fclose (varFileP);
+
+    FILE * maskFileP;
+    maskFileP = fopen ("./warpRawData/input.mask", "wb");
+    fwrite (maskBuffer , sizeof(uint16_t), width*height, maskFileP);
+    fclose (maskFileP);
+
+    cout << sizeof(float)*width*height << " bytes" << endl;
+
+    free(imageBuffer);
+    free(varianceBuffer);
+    free(maskBuffer);
+}
+
+
+
+
+
+
+
